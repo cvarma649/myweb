1d722fdb243bef5f60db4a841b92db7955452a66
cvarma649 <75749697+cvarma649@users.noreply.github.com>
2021-06-28 14:02:31 +0530

    changed email to req.body.crude_email

--- newapp/App.js
+++ newapp/App.js
@@ -62,7 +62,7 @@ app.use(function (req, res, next) {
 
 app.post("/api/v1/signup", urlencoder, async(req,res, next)=>{
      const {name, crude_email, password} = req.body
-    const email = (req.crude_email).toLowerCase();
+    const email = (req.body.crude_email).toLowerCase();
     const validateEmail=(email)=>{
         const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         return  re.test(String(email));}
