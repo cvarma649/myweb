4d7b6bd88ea43f646d0407b4b4b62c4400797011
cvarma649 <75749697+cvarma649@users.noreply.github.com>
2021-06-28 14:27:42 +0530

    changed res.status.json to res.json in signin

--- newapp/App.js
+++ newapp/App.js
@@ -157,12 +157,12 @@ app.post("/api/v1/signin",  async(req,res)=>{
     try {
         const user = await pool.query("SELECT * FROM users WHERE email = $1", [email])
         if(user.rows.length===0){
-            res.status(200).json("Sign Up First!");
+            res.json("Sign Up First!");
         }
         else{ 
             const validPassword = await bcrypt.compare(password, user.rows[0].password);
             if (!validPassword){
-            return res.status(201).json("Invalid Credentials")
+            return res.json("Invalid Credentials")
             }
             else{
             const token = jwtGenerator(user.rows[0].user_id)
